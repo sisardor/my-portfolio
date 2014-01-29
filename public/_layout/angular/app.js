c74e8380c9db23ddeffdf9ed6630750cdb2533d9
Sardor Isakov <sisardor@gmail.com>
2014-01-28 21:59:23 -0800

    New updates

--- public/_layout/angular/app.js
+++ public/_layout/angular/app.js
@@ -1,7 +1,17 @@
 (function (angular, undefined) { 'use strict';
   
-  angular.module('resumeApp', [])
-  .config(function () {
+  angular.module('resumeApp', ['ngRoute','ngAnimate', 'ngTouch'])
+  .config(function ($routeProvider) {
+     $routeProvider.
+        when('/', { 
+          templateUrl: '_layout/angular/templates/item-list.html', 
+          controller: 'MainCTRL'
+        }).
+        when('/project/:id', { 
+          templateUrl: '_layout/angular/templates/item-detail.html', 
+          controller: 'ViewProjectCTRL'
+        }).
+        otherwise({redirectTo: '/'});
 
   })
   .controller('MainCTRL', function($scope){
@@ -42,5 +52,42 @@
     $scope.projects = tmp;
 
   })
+  .controller('ViewProjectCTRL', function($scope,$timeout){
+// Set of Photos
+    $scope.photos = [
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 01'},
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 02'},
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 03'},
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 04'},
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 05'},
+        {src: '_content/438x202-placeholder.jpg', desc: 'Image 06'}
+    ];
+
+    // initial image index
+    $scope._Index = 0;
+
+    // if a current image is the same as requested image
+    $scope.isActive = function (index) {
+        return $scope._Index === index;
+    };
+
+    // show prev image
+    $scope.showPrev = function () {
+        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
+    };
+
+    // show next image
+    $scope.showNext = function () {
+        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
+    };
+
+    // show a certain image
+    $scope.showPhoto = function (index) {
+        $scope._Index = index;
+    };
+      
+
+  })
+ 
 
 })(window.angular);
