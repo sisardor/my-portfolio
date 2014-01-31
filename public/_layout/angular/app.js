ef431c3941e0232aa79baa62fcde3ce5434ef47f
Sardor Isakov <sisardor@gmail.com>
2014-01-31 02:09:49 -0800

    Added pagination

--- public/_layout/angular/app.js
+++ public/_layout/angular/app.js
@@ -1,9 +1,13 @@
 (function (angular, undefined) { 'use strict';
   
-  angular.module('resumeApp', ['ngRoute','ngAnimate', 'ngTouch'])
+  angular.module('resumeApp', ['ngRoute','ngAnimate', 'ngTouch','ngSanitize'])
   .config(function ($routeProvider) {
      $routeProvider.
         when('/', { 
+          templateUrl: '_layout/angular/templates/item-list.html', 
+          controller: 'MainCTRL'
+        }).
+        when('/page/:num', { 
           templateUrl: '_layout/angular/templates/item-list.html', 
           controller: 'MainCTRL'
         }).
@@ -14,42 +18,61 @@
         otherwise({redirectTo: '/'});
 
   })
-  .controller('MainCTRL', function($scope){
-    console.log("MainCTRL")
+  
+  .controller('MainCTRL', function($scope,$route){
+    //console.log("MainCTRL")
     var tmp = [
       {
         id:'item1',
         title:'Lorem ipsum dolor',
         description:'Etiam vitae justo ut felis ultrices placerat. Mauris dictum tellus ac dui bibendum at viverra augue iaculis. Praesent accumsan purus id ante.', 
-        image:'_content/216x141-placeholder.jpg'
+        image:'_content/216x141-placeholder.jpg',
+        link:'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>'
       },
       {
         id:'item2',
         title:'Lorem ipsum dolor',
         description:'Etiam vitae justo ut felis ultrices placerat. Mauris dictum tellus ac dui bibendum at viverra augue iaculis. Praesent accumsan purus id ante.', 
-        image:'_content/216x141-placeholder.jpg'
+        image:'_content/216x141-placeholder.jpg',
+        link:'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>'
       },
       {
         id:'item3',
         title:'Lorem ipsum dolor',
         description:'Etiam vitae justo ut felis ultrices placerat. Mauris dictum tellus ac dui bibendum at viverra augue iaculis. Praesent accumsan purus id ante.', 
-        image:'_content/216x141-placeholder.jpg'
+        image:'_content/216x141-placeholder.jpg',
+        link:'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>'
       },
       {
         id:'item4',
         title:'Lorem ipsum dolor',
         description:'Etiam vitae justo ut felis ultrices placerat. Mauris dictum tellus ac dui bibendum at viverra augue iaculis. Praesent accumsan purus id ante.', 
-        image:'_content/216x141-placeholder.jpg'
+        image:'_content/216x141-placeholder.jpg',
+        link:'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>'
       },
       {
         id:'item5',
         title:'Lorem ipsum dolor',
         description:'Etiam vitae justo ut felis ultrices placerat. Mauris dictum tellus ac dui bibendum at viverra augue iaculis. Praesent accumsan purus id ante.', 
-        image:'_content/216x141-placeholder.jpg'
+        image:'_content/216x141-placeholder.jpg',
+        link:'I am an <code>HTML</code>string with <a href="#">links!</a> and other <em>stuff</em>'
       }
     ];
 
-    $scope.projects = tmp;
+    var page = $route.current.params.num || 1;
+    $scope.currentPage = function(num){
+      return (num == page);
+    };
+
+    $scope.projects = [];
+    var itemPerPage = 3;
+    var ii = page * itemPerPage;
+    for (var i = ii-itemPerPage; i < ii; i++) {
+      if(i > tmp.length - 1 ) {
+        break;
+      }
+      $scope.projects.push(tmp[i])
+    };
 
   })
   .controller('ViewProjectCTRL', function($scope,$timeout){
@@ -85,6 +108,8 @@
     $scope.showPhoto = function (index) {
         $scope._Index = index;
     };
+
+    $scope.projectDetails = '<br /><p><strong>Project Details:</strong></p><ul><li>Donec imperdiet nunc</li><li>Id felis faucibus eu tempor</li><li>Sem tincidunt fusce a orci in</li><li>Risus ultrices pharetra</li></ul><p><strong>Live preview:</strong><br /><a href="#">www.sitelink.com</a></p>';
       
 
   })
