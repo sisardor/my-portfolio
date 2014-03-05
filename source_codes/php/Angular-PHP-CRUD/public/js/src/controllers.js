710765410f5d99eaccdf9966f2690b6675b4ea73
Sardor Isakov <sisardor@gmail.com>
2014-03-04 23:20:25 -0800

    More deleted files

--- source_codes/php/Angular-PHP-CRUD/public/js/src/controllers.js
+++ source_codes/php/Angular-PHP-CRUD/public/js/src/controllers.js
@@ -1,229 +1,205 @@
+/**
+ * controllers.js v1.0.0 by Sardor Isakov
+ * Copyright 2013 Sardor Isakov
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @name controllers
+ *
+ * @description
+ * AngularJS module, controllers which are used in account page
+ */
+(function (document, angular, undifined) { 'use strict';
 
-  "use strict";
-/* Controllers */
-
-angular.module('myApp.controllers', ['ngResource'])
- /*===========Single controller=============*/
-  .controller('ContactsSingleCTRL', function($scope, $resource, $routeParams, Contact, $location) {
-
-    $scope.contact = Contact.get({ name: $routeParams.name}, function() {
-      // Success
-      $scope.legend_title = "Editing " +  $scope.contact.name.first +" "+ $scope.contact.name.last;
-    }, function(response){
-        // Error
-        if(response.status === 404) {
-          console.log(response.data);
-          $location.path('contacts');
-          
-        }
-
-    });
-
-    $scope.save = function() {
-      $scope.contact.$save(function(updated_contact){
-        $scope.contact = updated_contact;
-        $location.path('/view/' + updated_contact.clean_name).replace();
-      });
-    };
-  })
-  /*===========Add controller=============*/
-  .controller('ContactsAddCTRL', function($scope, $resource, Contact, $location) {
-  	$scope.legend_title = "New contact"
-    $scope.contact = new Contact({});
-    $scope.contact.added = new Date();
-    $scope.save = function() {
-      $scope.contact.$add(function(){
-        $location.path('contacts');
-      });
-    };
-  })
-	.controller('ContactsCTRL', function($scope, Contact) {
+	/* Controllers */
+	angular.module('myApp.controllers', ['ngResource'])
+	/*===========Single controller=============*/
+	.controller('ContactsSingleCTRL',['$scope', '$routeParams','Contact', '$location', function ($scope, $routeParams, Contact, $location) {
+		$scope.contact = Contact.get({
+			name: $routeParams.name
+		}, function () {
+			// Success
+			$scope.legend_title = "Editing " + $scope.contact.name.first + " " + $scope.contact.name.last;
+		}, function (response) {
+			// Error
+			if (response.status === 404) {
+				console.log(response.data);
+				$location.path('contacts');
+			}
+		});
+		$scope.save = function () {
+			$scope.contact.$save(function (updated_contact) {
+				$scope.contact = updated_contact;
+				$location.path('/view/' + updated_contact.clean_name).replace();
+			});
+		};
+	}])
+	/*===========Add controller=============*/
+	.controller('ContactsAddCTRL',['$scope', '$resource', 'Contact', '$location', function ($scope, $resource, Contact, $location) {
+		$scope.legend_title = "New contact"
+		$scope.contact = new Contact({});
+		$scope.contact.added = new Date();
+		$scope.save = function () {
+			$scope.contact.$add(function () {
+				$location.path('contacts');
+			});
+		};
+	}])
+	.controller('ContactsCTRL', ['$scope', 'Contact', function ($scope, Contact) {
 		console.log('ContactsCTRL');
 		$scope.contacts = Contact.query();
-	 })
-	.controller('EventListen',  function($scope, $location, eventData) {
+	}])
+	.controller('EventListen', ['$scope', '$location', 'eventData', function ($scope, $location, eventData) {
 		$scope.events = eventData.getAllEvents();
-
-		$scope.navigateToDetails = function(event) {
-			$location.url('/event/'+event.id);
+		$scope.navigateToDetails = function (event) {
+			$location.url('/event/' + event.id);
 		}
-
-	})
-	.controller('MainCtrl',  ['$scope', '$http', 'myCache', function($scope, $http,myCache) {
+	}])
+	.controller('MainCtrl', ['$scope', '$http', 'myCache',function ($scope, $http, myCache) {
 		var stat = "pulled from Cache";
 		$scope.ads = myCache.get("ads");
-		if(!$scope.ads) {
+		if (!$scope.ads) {
 			stat = "pulled from Server";
-
 			$http.get('account/ads')
-			  .success(function (data) {
-			    $scope.ads = data; // { foo: 'bar' }
-			    myCache.put("ads",data);
-			})
-			  .error(function (data, status, headers, config) {
-			    // handle error
-			});
+				.success(function (data) {
+					$scope.ads = data; // { foo: 'bar' }
+					myCache.put("ads", data);
+				})
+				.error(function (data, status, headers, config) {
+					// handle error
+				});
 		}
-		var info = "[Cache info capacity: " + myCache.info().capacity +  " size: " + myCache.info().size + "]"
+		var info = "[Cache info capacity: " + myCache.info().capacity + " size: " + myCache.info().size + "]"
 		console.log(stat + "     " + document.URL + "    " + info);
 	}])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	.controller('UploadCtrl',  function($scope,$http,$timeout, $upload) {
+	.controller('UploadCtrl', ['$scope', '$http', '$timeout', '$upload', function ($scope, $http, $timeout, $upload) {
 		$scope.products = [];
 		$scope.product = {};
 		$scope.save = function (data) {
-		    $scope.product = angular.copy(data);
-		    $http.post('/api/upload', $scope.product)
-		        .success(function () {
-		            window.uploadClicked(); 
-		            console.log("from here");
-		        })
-		        .error(function (data) {
-		           // alert(data);
-		        });
-		    }
-
-
+			$scope.product = angular.copy(data);
+			$http.post('/api/upload', $scope.product)
+				.success(function () {
+					window.uploadClicked();
+					console.log("from here");
+				})
+				.error(function (data) {
+					// alert(data);
+				});
+		}
 		$scope.fileReaderSupported = window.FileReader != null;
 		$scope.uploadRightAway = true;
-		$scope.changeAngularVersion = function() {
+		$scope.changeAngularVersion = function () {
 			window.location.hash = $scope.angularVersion;
 			window.location.reload(true);
 		}
-		$scope.hasUploader = function(index) {
+		$scope.hasUploader = function (index) {
 			return $scope.upload[index] != null;
 		};
 		function setPreview(fileReader, index) {
-			fileReader.onload = function(e) {
-			    $timeout(function() {
-			        $scope.dataUrls[index] = e.target.result;
-			    });
+			fileReader.onload = function (e) {
+				$timeout(function () {
+					$scope.dataUrls[index] = e.target.result;
+				});
 			}
 		}
 		$scope.angularVersion = window.location.hash.length > 1 ? window.location.hash.substring(1) : '1.2.0';
-		$scope.onFileSelect = function($files) {
-			    //$files: an array of files selected, each file has name, size, and type.
-		    for (var i = 0; i < $files.length; i++) {
-		      var file = $files[i];
-		      $scope.upload = $upload.upload({
-		        url: 'api/upload', //upload.php script, node.js route, or servlet url
-		        // method: POST or PUT,
-		        // headers: {'headerKey': 'headerValue'},
-		        // withCredentials: true,
-		        data: {myObj: $scope.myModel},
-		        file: file,
-		        fileFormDataName: 'image'
-		        // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
-		        /* set file formData name for 'Content-Desposition' header. Default: 'file' */
-		        //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
-		        /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
-		        //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
-		      }).progress(function(evt) {
-		        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-		      }).success(function(data, status, headers, config) {
-		        // file is uploaded successfully
-		        console.log(data);
-		      });
-		      //.error(...)
-		      //.then(success, error, progress);
+		$scope.onFileSelect = function ($files) {
+			//$files: an array of files selected, each file has name, size, and type.
+			for (var i = 0; i < $files.length; i++) {
+				var file = $files[i];
+				$scope.upload = $upload.upload({
+					url: 'api/upload', //upload.php script, node.js route, or servlet url
+					// method: POST or PUT,
+					// headers: {'headerKey': 'headerValue'},
+					// withCredentials: true,
+					data: {
+						myObj: $scope.myModel
+					},
+					file: file,
+					fileFormDataName: 'image'
+					// file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+					/* set file formData name for 'Content-Desposition' header. Default: 'file' */
+					//fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+					/* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+					//formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+				}).progress(function (evt) {
+					console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+				}).success(function (data, status, headers, config) {
+					// file is uploaded successfully
+					console.log(data);
+				});
 			}
 		}
-
-	})
-
-	.controller('AddCtrl',  ['$scope', '$http', function($scope, $http) {
-		$scope.options = [
-		    {category:'коммерческая', group:'недвижимость', value:'nko'},
-		    {category:'гаражи и стоянки', group:'недвижимость',value:'nga'},
-		    {category:'автомобили - частные', group:'транспорт',value:'aup'},
-		    {category:'автомобили - дилерские', group:'транспорт',value:'aud'},
-		    {category:'события', group:'разное',value:'rso'}
-	  	];
-	  	$scope.templates = [ 
-	  		{ name: 'form_nko.html', url: 'form_nko.html'},
-			{ name: 'form_aup.html', url: 'form_aup.html'} 
-		];
-	  	$scope.option = $scope.options; // red
-	  	$scope.category = "ngo";
-	  	$scope.$watch("option", function( selected ) {
-			if ( selected.value === "nko" || selected.value === "nga" ) {
-				$scope.template = $scope.templates[0];
-				$scope.category = selected.value;
-				$scope.partial_clear();
-				console.log('nko');
-
-			}
-			if ( selected.value === "aup" || selected.value === "aud" ) {
-				$scope.template = $scope.templates[1];
-				$scope.category = selected.value;
-				$scope.partial_clear();
-				console.log('aup');
-			}
-			if ( selected.value === "rso" ) {
-				$scope.template = null;
-				$scope.category = selected.value;
-				$scope.partial_clear();
-			}
-		});
-		$scope.master = {};
-		$scope.update = function(ad) {
-			$scope.master= angular.copy(ad);
-			console.log($scope.master);
-			$scope.master.cat = $scope.category;
-
-			$http({
-				url 	: '/account/ads',
-				method	: 'POST',
-				data	: $scope.master
-			}).error(function(data,status, headers,config){
-				console.log("Error!!");
-			});
-		};
-	 
-		$scope.reset = function() {
-			$scope.ad = angular.copy($scope.master); 
-		};
-		$scope.numberOfRooms = function(num) {
-			$scope.ad.rooms = num;
-		};
-
-		$scope.partial_clear = function() {
-			$scope.master = {};
-			var tmp   = new Object();
-			tmp.title = $scope.ad.title;
-			tmp.price = $scope.ad.price;
-			tmp.text  = $scope.ad.text;
-			$scope.ad = angular.copy(tmp);
-		};
-			 
-		$scope.reset();
 	}])
-
-	.controller('EditCtrl',  ['$scope', '$routeParams', '$location', '$http', 'Content', 'myCache', function($scope, $routeParams, $location, $http, Content, myCache) {
+	.controller('AddCtrl', ['$scope', '$http',
+		function ($scope, $http) {
+			$scope.options = [
+				{category: 'коммерческая',group: 'недвижимость',value: 'nko'},
+				{category: 'гаражи и стоянки',group: 'недвижимость',value: 'nga'},
+				{category: 'автомобили - частные',group: 'транспорт',value: 'aup'},
+				{category: 'автомобили - дилерские',group: 'транспорт',value: 'aud'},
+				{category: 'события',group: 'разное',value: 'rso'}
+	  		];
+			$scope.templates = [
+				{
+					name: 'form_nko.html',
+					url: 'assets/account/form_nko.html'
+				},
+				{
+					name: 'form_aup.html',
+					url: 'assets/account/form_aup.html'
+				}
+			];
+			$scope.option = $scope.options; // red
+			$scope.category = "ngo";
+			$scope.$watch("option", function (selected) {
+				if (selected.value === "nko" || selected.value === "nga") {
+					$scope.template = $scope.templates[0];
+					$scope.category = selected.value;
+					$scope.partial_clear();
+					console.log('nko');
+				}
+				if (selected.value === "aup" || selected.value === "aud") {
+					$scope.template = $scope.templates[1];
+					$scope.category = selected.value;
+					$scope.partial_clear();
+					console.log('aup');
+				}
+				if (selected.value === "rso") {
+					$scope.template = null;
+					$scope.category = selected.value;
+					$scope.partial_clear();
+				}
+			});
+			$scope.master = {};
+			$scope.update = function (ad) {
+				$scope.master = angular.copy(ad);
+				console.log($scope.master);
+				$scope.master.cat = $scope.category;
+				$http({
+					url: '/account/ads',
+					method: 'POST',
+					data: $scope.master
+				}).error(function (data, status, headers, config) {
+					console.log("Error!!");
+				});
+			};
+			$scope.reset = function () {
+				$scope.ad = angular.copy($scope.master);
+			};
+			$scope.numberOfRooms = function (num) {
+				$scope.ad.rooms = num;
+			};
+			$scope.partial_clear = function () {
+				$scope.master = {};
+				var tmp = new Object();
+				tmp.title = $scope.ad.title;
+				tmp.price = $scope.ad.price;
+				tmp.text = $scope.ad.text;
+				$scope.ad = angular.copy(tmp);
+			};
+			$scope.reset();
+	}])
+	.controller('EditCtrl',  ['$scope', '$routeParams', '$location', '$http', 'Content', 'myCache', function ($scope, $routeParams, $location, $http, Content, myCache) {
 		$scope.options = [
 		    {category:'коммерческая', group:'недвижимость', value:'nko',isinuse: true},
 		    {category:'гаражи и стоянки', group:'недвижимость',value:'nga',isinuse: true},
@@ -233,8 +209,8 @@ angular.module('myApp.controllers', ['ngResource'])
 	  	];
 
 	  	$scope.templates = [ 
-	  		{ name: 'form_nko.html', url: 'form_nko.html'},
-			{ name: 'form_aup.html', url: 'form_aup.html'} 
+	  		{ name: 'form_nko.html', url: 'assets/account/form_nko.html'},
+			{ name: 'form_aup.html', url: 'assets/account/form_aup.html'} 
 		];
 	  	$scope.option = $scope.options; // red
 
@@ -322,34 +298,62 @@ angular.module('myApp.controllers', ['ngResource'])
 		};
 
 		//$scope.reset();
-	}]);
+	}])
+    .controller('ProfileImageCTRL', ['$scope', '$http', '$timeout', '$upload', function ($scope, $http, $timeout, $upload) {
+        $http.get('profile')
+            .success(function (data) {
+                $scope.user = data;
+            })
+        console.log('ProfileImageCTRL')
+        $scope.fileReaderSupported = window.FileReader != null;
+        $scope.uploadRightAway = true;
+        $scope.changeAngularVersion = function () {
+            window.location.hash = $scope.angularVersion;
+            window.location.reload(true);
+        }
+        $scope.hasUploader = function (index) {
+            return $scope.upload[index] != null;
+        };
+
+        function setPreview(fileReader, index) {
+            fileReader.onload = function (e) {
+                $timeout(function () {
+                    $scope.dataUrls[index] = e.target.result;
+                });
+            }
+        }
+        $scope.angularVersion = window.location.hash.length > 1 ? window.location.hash.substring(1) : '1.2.0';
+        $scope.onFileSelect = function ($files) {
+            //$files: an array of files selected, each file has name, size, and type.
+            for (var i = 0; i < $files.length; i++) {
+                var file = $files[i];
+                $scope.upload = $upload.upload({
+                    url: 'api/upload', //upload.php script, node.js route, or servlet url
+                    // method: POST or PUT,
+                    // headers: {'headerKey': 'headerValue'},
+                    // withCredentials: true,
+                    data: {
+                        myObj: $scope.myModel
+                    },
+                    file: file,
+                    fileFormDataName: 'image'
+                    // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+                    /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+                    //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+                    /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+                    //formDataAppender: function(formData, key, val){} //#40#issuecomment-28612000
+                }).progress(function (evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function (data, status, headers, config) {
+                    $scope.user.avatar = data.avatar;
+                    // file is uploaded successfully
+                    console.log(data);
+                });
+                //.error(...)
+                //.then(success, error, progress);
+            }
+        }
+    }]);
 
 
-
-function Single($scope, $routeParams, datasets, myCache) {
-	$scope.ad = datasets;
-};
-
-Single.resolve = {
-	datasets : function($q, $route, $timeout, Phone) {
-	var deferred = $q.defer();
-	var name = $route.current.params.id;
-
-	var successCb = function(result) {
-	    if(angular.equals(result, [])) {
-	     	console.log("No starship found by that id");
-	     	deferred.reject("Not found");
-	    } else {
-	     	console.log("found");
-	     	deferred.resolve(result);
-	     	}
-	    };
-
-	    $timeout(function() {
-	     	Phone.get($route.current.params.id, successCb);
-	     		//Phone.query(successCb);
-	    }, 200)
-
-	    return deferred.promise;
-	}
-};
+})(document, window.angular); // END (function () {}
